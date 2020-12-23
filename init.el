f0b39b029966dab07422cf94c4839a65678614cb
Matt Trzcinski <mtrzcinski@ushrauto.com>
2020-12-23 11:10:00 -0500

    Experimental: quick-bind

--- init.el
+++ init.el
@@ -1443,3 +1443,26 @@ chicken and egg problem."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; experimental
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (defvar xc/quick-bind nil
+;;   "Quick binding for F5 key.
+
+;; Must be a function that takes no arguments.")
+
+;; (defun xc/set-quick-bind ()
+;;   "Set function to `xc/quick-bind'."
+;;   (interactive)
+;;   (let ((func (intern (completing-read "Set `xc/quick-bind' to: " obarray #'functionp))))
+;;     (fset 'xc/quick-bind (symbol-function 'func))
+;;     (message "Set `xc/quick-bind' to %s" func)))
+
+;; (defun xc/test ()
+;;   "Say hello"
+;;   (message "Hello!"))
+
+;; (funcall
+;; (lambda () (interactive)
+;;                 (let ((func (symbol-function xc/quick-bind)))
+;;                   (if (commandp func)
+;;                       (call-interactively func)
+;;                     (funcall func)))))
