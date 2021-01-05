fcc328c04cd8d265747c54e642ecdad02b891412
Matt Trzcinski <mtrzcinski@ushrauto.com>
2021-01-05 09:46:47 -0500

    Create experimetal lisp laboratory

---
+++ experimental.el
@@ -0,0 +1,58 @@
+;;; experimental.el -- Lisp Laboratory
+
+;; Develop Lisp extensions prior to incorporation into init.el or a
+;; separate package
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; - yank to mark, set mark, move to other location (other window or
+;; within buffer), kill something, yank to the marked location
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; quick bind
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defvar xc/quick-bind nil
+;;   "Quick binding for F5 key.
+
+;; Must be a function that takes no arguments.")
+
+;; (defun xc/set-quick-bind ()
+;;   "Set function to `xc/quick-bind'."
+;;   (interactive)
+;;   (let ((func (intern (completing-read "Set `xc/quick-bind' to: " obarray #'functionp))))
+;;     (fset 'xc/quick-bind (symbol-function 'func))
+;;     (message "Set `xc/quick-bind' to %s" func)))
+
+;; (defun xc/test ()
+;;   "Say hello"
+;;   (message "Hello!"))
+
+;; (funcall
+;; (lambda () (interactive)
+;;                 (let ((func (symbol-function xc/quick-bind)))
+;;                   (if (commandp func)
+;;                       (call-interactively func)
+;;                     (funcall func)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; add fixed case abbrev
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defun my-add-case-fixed-abbrev (table name expansion)
+;;   "Add abbrev with case-fixed t property."
+;;   (interactive "XAbbrev table (global-abbrev-table): "
+;;    (let ((table (intern-soft (completing-read
+;;           "Abbrev table (global-abbrev-table): "
+;;           abbrev-table-name-list nil t nil nil "global-abbrev-table" )))
+;;          (name (read-string "Abbrev name: "))
+;;          (expansion (read-string "Expansion: ")))
+;;      (list name expansion table)))
+;;   (let ((table (or table global-abbrev-table)))
+;;     (define-abbrev table name expansion nil :case-fixed t)))
+
+;; (eq 'global-abbrev-table (intern-soft "global-abbrev-table"))
+
+;; (define-abbrev (intern-soft "global-abbrev-table") "duder" "man" nil :case-fixed t)
+
+;; (my-add-case-fixed-abbrev "my-abbrev" "my-expansion" "global-abbrev-table")
+;; (write-abbrev-file)
--- init.el
+++ init.el
@@ -436,12 +436,13 @@ permanent binding.")
     (general-def
       :keymaps 'override
       :prefix "C-x i"
+      "a" '(lambda () (interactive) (find-file "~/.emacs.d/archive/andr-init.el"))
       "i" '(lambda () (interactive) (find-file "~/.emacs.d/init.el"))
       "b" '(lambda () (interactive) (find-file "~/.emacs.d/brag.org"))
-      "g" '(lambda () (interactive) (find-file "~/.emacs.d/encyclopedia.org"))
-      "n" '(lambda () (interactive) (find-file "~/.emacs.d/notes.org"))
       "c" '(lambda () (interactive) (find-file "~/.emacs.d/archive/classic-init.el"))
-      "a" '(lambda () (interactive) (find-file "~/.emacs.d/archive/andr-init.el"))
+      "e" '(lambda () (interactive) (find-file "~/.emacs.d/experimental.el"))
+      "g" '(lambda () (interactive) (find-file "~/.emacs.d/glossary.org"))
+      "n" '(lambda () (interactive) (find-file "~/.emacs.d/notes.org"))
       "p" '(lambda () (interactive) (find-file "~/peut-gerer-projects.el"))
       )
 
@@ -1488,54 +1489,3 @@ chicken and egg problem."
   (interactive)
   (insert "\"C:\\python\\python37\\python.exe\" -m venv venv"))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; experimental
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; idea
-;;
-;; - yank to mark, set mark, move to other location (other window or
-;; within buffer), kill something, yank to the marked location
-
-;; (defvar xc/quick-bind nil
-;;   "Quick binding for F5 key.
-
-;; Must be a function that takes no arguments.")
-
-;; (defun xc/set-quick-bind ()
-;;   "Set function to `xc/quick-bind'."
-;;   (interactive)
-;;   (let ((func (intern (completing-read "Set `xc/quick-bind' to: " obarray #'functionp))))
-;;     (fset 'xc/quick-bind (symbol-function 'func))
-;;     (message "Set `xc/quick-bind' to %s" func)))
-
-;; (defun xc/test ()
-;;   "Say hello"
-;;   (message "Hello!"))
-
-;; (funcall
-;; (lambda () (interactive)
-;;                 (let ((func (symbol-function xc/quick-bind)))
-;;                   (if (commandp func)
-;;                       (call-interactively func)
-;;                     (funcall func)))))
-
-;; (defun my-add-case-fixed-abbrev (table name expansion)
-;;   "Add abbrev with case-fixed t property."
-;;   (interactive "XAbbrev table (global-abbrev-table): "
-;;    (let ((table (intern-soft (completing-read
-;;           "Abbrev table (global-abbrev-table): "
-;;           abbrev-table-name-list nil t nil nil "global-abbrev-table" )))
-;;          (name (read-string "Abbrev name: "))
-;;          (expansion (read-string "Expansion: ")))
-;;      (list name expansion table)))
-;;   (let ((table (or table global-abbrev-table)))
-;;     (define-abbrev table name expansion nil :case-fixed t)))
-
-;; (eq 'global-abbrev-table (intern-soft "global-abbrev-table"))
-
-;; (define-abbrev (intern-soft "global-abbrev-table") "duder" "man" nil :case-fixed t)
-
-;; (my-add-case-fixed-abbrev "my-abbrev" "my-expansion" "global-abbrev-table")
-;; (write-abbrev-file)
